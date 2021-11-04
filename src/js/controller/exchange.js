d32fd4a43746aac28311ccc390eb604899b1d93a
joker77z <joker77z@naver.com>
2021-11-04 21:10:42 +0900

    feat: Modify hotplace, inputbox style, ratio -0.00

--- src/js/controller/exchange.js
+++ src/js/controller/exchange.js
@@ -1,11 +1,4 @@
-import {
-  country,
-  beforeCountry,
-  beforeMoney,
-  invertExchangeRender,
-  exchangeRender,
-  hotplaceRender,
-} from '../view/exchange';
+import { country, beforeCountry, beforeMoney, exchangeRender, hotplaceRender } from '../view/exchange';
 
 const exchangeSection = document.querySelector('.exchange__section');
 
@@ -13,13 +6,9 @@ const exchangeSection = document.querySelector('.exchange__section');
 window.addEventListener('DOMContentLoaded', () => {
   exchangeRender();
   hotplaceRender();
-
-  invertExchangeRender(50000, '미국');
 });
 
 exchangeSection.addEventListener('change', () => {
-  // 없는 국가이거나 돈이 비어있다면 return
   if (!Object.keys(country).includes(beforeCountry.value) && beforeMoney) return;
-  // 국가도 써있고 돈도 입력했다면 렌더
   exchangeRender();
 });
